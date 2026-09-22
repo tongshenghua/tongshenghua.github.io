48cf3aa3b5d852ffcdce7680f772a6b5456404cb
tongshenghua <2542193800@qq.com>
2026-09-22 13:43:37 +0800

    Site updated: 2026-09-22 13:43:36

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/08/02/hello-world/"];function toRandomPost(){
+var posts=["2026/08/02/hello-world/","2026/09/22/javascript-prototype-pollution/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
